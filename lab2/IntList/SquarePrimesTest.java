2431c56d4b59c6047450e75caeddd2bc3decc960
meter <1462068546@qq.com>
2025-05-29 17:44:13 +0800

    done with lab2

--- lab2/IntList/SquarePrimesTest.java
+++ lab2/IntList/SquarePrimesTest.java
@@ -17,4 +17,26 @@ public class SquarePrimesTest {
         assertEquals("14 -> 15 -> 16 -> 289 -> 18", lst.toString());
         assertTrue(changed);
     }
+
+    /**
+     * 没有质数，changed应该为false
+     */
+    @Test
+    public void testSquarePrimesComplex1() {
+        IntList lst = IntList.of(14, 15, 16, 16, 18);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("14 -> 15 -> 16 -> 16 -> 18", lst.toString());
+        assertFalse(changed);
+    }
+
+    /**
+     * 有多个质数，changed应该为true
+     */
+    @Test
+    public void testSquarePrimesComplex2() {
+        IntList lst = IntList.of(2, 3, 16, 17, 18);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("4 -> 9 -> 16 -> 289 -> 18", lst.toString());
+        assertTrue(changed);
+    }
 }
