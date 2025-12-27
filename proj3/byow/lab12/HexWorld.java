8eb33a0be87a3ef1e2e13e9eb900867190078341
meter <1462068546@qq.com>
2025-12-27 22:16:10 +0800

    done with lab12

--- proj3/byow/lab12/HexWorld.java
+++ proj3/byow/lab12/HexWorld.java
@@ -1,6 +1,4 @@
 package byow.lab12;
-import org.junit.Test;
-import static org.junit.Assert.*;
 
 import byow.TileEngine.TERenderer;
 import byow.TileEngine.TETile;
@@ -12,4 +10,79 @@ import java.util.Random;
  * Draws a world consisting of hexagonal regions.
  */
 public class HexWorld {
+
+    private static final int WIDTH = 40;
+    private static final int HEIGHT = 40;
+
+    private static final long SEED = 28731223;
+    private static final Random RANDOM = new Random(SEED);
+
+    /**
+     * Picks a RANDOM tile.
+     */
+    private static TETile randomTile() {
+        int tileNum = RANDOM.nextInt(5);
+        switch (tileNum) {
+            case 0:
+                return Tileset.GRASS;
+            case 1:
+                return Tileset.FLOWER;
+            case 2:
+                return Tileset.SAND;
+            case 3:
+                return Tileset.TREE;
+            case 4:
+                return Tileset.MOUNTAIN;
+            default:
+                return Tileset.NOTHING;
+        }
+    }
+
+    /**
+     * Fills the given 2D array of tiles with a single hexagon.
+     */
+    private static void fillSingleHexagon(TETile[][] tiles, int xPos, int yPos, int size) {
+        TETile tile = randomTile();
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size + 2 * i; j++) {
+                tiles[xPos - i + j][yPos + i] = TETile.colorVariant(tile,
+                        10, 10, 10, RANDOM);
+                tiles[xPos - i + j][yPos + 2 * size - 1 - i] = TETile.colorVariant(tile,
+                        10, 10, 10, RANDOM);
+            }
+        }
+    }
+
+    /**
+     * Fills the given 2D array of tiles with a hexagonal tessellation.
+     */
+    private static void fillHexagonWorld(TETile[][] tiles, int xPos, int yPos, int size) {
+        // fill middle column
+        for (int i = 0; i < 5; i++) {
+            fillSingleHexagon(tiles, xPos, yPos + i * size * 2, size);
+        }
+        for (int i = 0; i < 2; i++) {
+            int dx = (size * 2 - 1) * (i + 1), dy = size * (i + 1);
+            for (int j = 0; j < 4 - i; j++) {
+                fillSingleHexagon(tiles, xPos - dx, yPos + dy + j * size * 2, size);
+                fillSingleHexagon(tiles, xPos + dx, yPos + dy + j * size * 2, size);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        TERenderer ter = new TERenderer();
+        ter.initialize(WIDTH, HEIGHT);
+
+        TETile[][] hexWorld = new TETile[WIDTH][HEIGHT];
+        for (int x = 0; x < WIDTH; x++) {
+            for (int y = 0; y < HEIGHT; y++) {
+                hexWorld[x][y] = Tileset.NOTHING;
+            }
+        }
+        int size = 4;
+        fillHexagonWorld(hexWorld, (WIDTH - size) / 2, 0, size);
+
+        ter.renderFrame(hexWorld);
+    }
 }
--- proj3/byow/lab12/project3prep.md
+++ proj3/byow/lab12/project3prep.md
@@ -5,6 +5,7 @@ After looking at your own implementation, consider the implementation provided n
 How did your implementation differ from the given one? What lessons can be learned from it?**
 
 Answer:
+The difference is that their implementation is recursive, while I used loops directly. Their implementation has more granular separation of functions, whereas in my implementation, the subtasks were not divided in such detail. My lesson is that dividing problems into simple, clean subtasks can reduce the difficulty of the overall problem.
 
 -----
 
@@ -12,14 +13,19 @@ Answer:
 What is the hexagon and what is the tesselation on the Project 3 side?**
 
 Answer:
+The hallways and rooms in Project 3 are analogous to hexagons. Filling rooms and hallways in an appropriate manner is analogous to the tessellation process.
 
 -----
-**If you were to start working on world generation, what kind of method would you think of writing first? 
+
+**If you were to start working on world generation, what kind of method would you think of writing first?
 Think back to the lab and the process used to eventually get to tessellating hexagons.**
 
 Answer:
+I would first consider how to generate rooms and hallways.
 
 -----
+
 **What distinguishes a hallway from a room? How are they similar?**
 
 Answer:
+They both have walls. Rooms can be much larger, while hallway width can only be 1 or 2.
